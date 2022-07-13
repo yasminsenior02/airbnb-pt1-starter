c4675149185ac80e425d9d879dbeb870f79737dd
yasminsenior02 <yasminsenior02@gmail.com>
2022-07-13 10:05:45 -0700

    dependencies

--- kavholm-express-api/config.js
+++ kavholm-express-api/config.js
@@ -1,32 +1,38 @@
-require("dotenv").config()
-require("colors")
+require("dotenv").config();
+require("colors");
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
-const SECRET_KEY = process.env.SECRET_KEY || "secret_dev"
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
+const SECRET_KEY = process.env.SECRET_KEY || "secret_dev";
 
-const IS_TESTING = process.env.NODE_ENV === "test"
+const IS_TESTING = process.env.NODE_ENV === "test";
 
 function getDatabaseUri() {
-  const dbUser = process.env.DATABASE_USER || "postgres"
-  const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
-  const dbHost = process.env.DATABASE_HOST || "localhost"
-  const dbPort = process.env.DATABASE_PORT || 5432
-  const dbTestName = process.env.DATABASE_TEST_NAME || "kavholm_test"
-  const dbProdName = process.env.DATABASE_NAME || "kavholm"
-  const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
+  const dbUser = process.env.DATABASE_USER || "postgres";
+  const dbPass = process.env.DATABASE_PASS
+    ? encodeURI(process.env.DATABASE_PASS)
+    : "postgres";
+  const dbHost = process.env.DATABASE_HOST || "localhost";
+  const dbPort = process.env.DATABASE_PORT || 5432;
+  const dbTestName =
+    process.env.DATABASE_TEST_NAME || "kavholm_test_advance_test";
+  const dbProdName = process.env.DATABASE_NAME || "kavholm_test_advance";
+  const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName;
 
-  return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  return (
+    process.env.DATABASE_URL ||
+    `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  );
 }
 
-const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13
+const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13;
 
-console.log("Kavholm Config:".red)
-console.log("PORT:".blue, PORT)
-console.log("SECRET_KEY:".blue, SECRET_KEY)
-console.log("IS_TESTING:".blue, IS_TESTING)
-console.log("BCRYPT_WORK_FACTOR".blue, BCRYPT_WORK_FACTOR)
-console.log("Database:".blue, getDatabaseUri())
-console.log("---")
+console.log("Advance Kavholm Config:".red);
+console.log("PORT:".blue, PORT);
+console.log("SECRET_KEY:".blue, SECRET_KEY);
+console.log("IS_TESTING:".blue, IS_TESTING);
+console.log("BCRYPT_WORK_FACTOR".blue, BCRYPT_WORK_FACTOR);
+console.log("Database:".blue, getDatabaseUri());
+console.log("---");
 
 module.exports = {
   PORT,
@@ -34,4 +40,4 @@ module.exports = {
   IS_TESTING,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
-}
+};
--- kavholm-express-api/kavholm.sql
+++ kavholm-express-api/kavholm.sql
@@ -1,9 +1,9 @@
 \echo 'Delete and recreate kavholm db?'
 \prompt 'Return for yes or control-C to cancel > ' answer
 
-DROP DATABASE kavholm;
-CREATE DATABASE kavholm;
-\connect kavholm
+DROP DATABASE kavholm_test_advance;
+CREATE DATABASE kavholm_test_advance;
+\connect kavholm_test_advance
 
 \i kavholm-schema.sql
 \i kavholm-seed.sql
@@ -11,9 +11,9 @@ CREATE DATABASE kavholm;
 \echo 'Delete and recreate kavholm_test db?'
 \prompt 'Return for yes or control-C to cancel > ' answer
 
-DROP DATABASE kavholm_test;
-CREATE DATABASE kavholm_test;
-\connect kavholm_test
+DROP DATABASE kavholm_test_advance_test;
+CREATE DATABASE kavholm_test_advance_test;
+\connect kavholm_test_advance_test
 
 \i kavholm-schema.sql
 \i kavholm-seed.sql
