2f69a32792ea9eeeb32494f37c19cd38f160146f
yasminsenior02 <yasminsenior02@gmail.com>
2022-07-13 10:02:40 -0700

    dependencies

--- kavholm-express-api/app.test.js
+++ kavholm-express-api/app.test.js
@@ -1,19 +1,21 @@
-const request = require("supertest")
-const app = require("./app")
-const db = require("./db")
+const request = require("supertest");
+const app = require("./app");
+const db = require("./db");
 
-test("not found for site 404", async () => {
-  const resp = await request(app).get("/no-such-path")
-  expect(resp.statusCode).toEqual(404)
-})
+describe("Test application", () => {
+  test("not found for site 404", async () => {
+    const resp = await request(app).get("/no-such-path");
+    expect(resp.statusCode).toEqual(404);
+  });
 
-test("not found for site 404 (test stack print)", async () => {
-  process.env.NODE_ENV = ""
-  const resp = await request(app).get("/no-such-path")
-  expect(resp.statusCode).toEqual(404)
-  delete process.env.NODE_ENV
-})
+  test("not found for site 404 (test stack print)", async () => {
+    process.env.NODE_ENV = "";
+    const resp = await request(app).get("/no-such-path");
+    expect(resp.statusCode).toEqual(404);
+    delete process.env.NODE_ENV;
+  });
+});
 
 afterAll(async () => {
-  await db.end()
-})
+  await db.end();
+});
